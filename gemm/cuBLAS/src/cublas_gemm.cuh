72f1bc0664bbe629430f83eeeb6ea66b8e97deee
haruhi55 <haruhi55@126.com>
2024-10-02 12:23:29 +0000

    sync with the master branch of TiledCUDA.

--- gemm/cuBLAS/src/cublas_gemm.cuh
+++ gemm/cuBLAS/src/cublas_gemm.cuh
@@ -30,8 +30,9 @@ void cublas_hgemm(int64_t kM, int64_t kN, int64_t kK,  // problem shape
         cudaDeviceSynchronize();
         time[0] = timer.stop() / iters;
     } else {
-        // C   = A @ B, but in cuBLAS, matrix is by default laid out in
-        // column-major C^T = B^T @ A^T [n, m] = [n, k] @ [k, m]
+        // C = A @ B, but in cuBLAS, matrix is by default laid out in
+        // column-major, therefore we compute:
+        // C^T = B^T @ A^T [n, m] = [n, k] @ [k, m]
         cublasHgemm(handle, CUBLAS_OP_T /* transb*/, CUBLAS_OP_N, kN, kM, kK,
                     &alf, B, kK, A, kK, &bet, C, kN);
         cudaDeviceSynchronize();
